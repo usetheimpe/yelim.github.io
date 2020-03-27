d436dfbd2d777c4eeb8f7d0a1d78d7cf711093bf
Yelim Kim <kim.yelim8@gmail.com>
2020-03-27 06:19:45 -0700

    masonry

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 (function ($) {
+    // var $grid = $('.grid').masonry({
+    //     itemSelector: '.grid-item',
+    //     percentPosition: true,
+    //     columnWidth: '.grid-sizer'
+    //   });
+    //   // layout Masonry after each image loads
+    //   $grid.imagesLoaded().progress( function() {
+    //     $grid.masonry();
+    //   });  
+      
     // Remove no-js class
     $('html').removeClass('no-js');
 
